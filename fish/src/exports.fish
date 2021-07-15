ff63e7ed68e8deea4ca1c7cc2ffcf0a4337883d0
Nick Balboni <nbalboni2@gmail.com>
2021-07-14 21:18:18 -0400

    Add JAVA_HOME env var

--- fish/src/exports.fish
+++ fish/src/exports.fish
@@ -3,6 +3,9 @@ set -gx EDITOR nano
 set -gx MAIN_DIR $HOME/Code
 set -gx DICTIONARY /usr/share/dict/words
 
+### ENVIRONMENTAL VARIABLES ####################################################
+set -gx JAVA_HOME /Library/Java/JavaVirtualMachines/adoptopenjdk-8.jdk/Contents/Home
+
 ### PATH #######################################################################
 set -gx PATH /usr/local/sbin $PATH # added for homebrew
 set -gx PATH ./node_modules/.bin $PATH # added for NPM (must be last)
