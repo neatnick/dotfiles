3fccb08c44787905a820558b0ec845ec63296a03
Nick Balboni <nbalboni2@gmail.com>
2022-01-20 23:57:01 -0500

    Update path configuration

--- fish/src/exports.fish
+++ fish/src/exports.fish
@@ -4,5 +4,6 @@ set -gx MAIN_DIR $HOME/Code
 set -gx DICTIONARY /usr/share/dict/words
 
 ### PATH #######################################################################
+set -gx PATH $PATH /Users/nick/Library/Python/3.9/bin # added python bin to the end for locally installed pip binaries
 set -gx PATH /usr/local/sbin $PATH # added for homebrew
-set -gx PATH ./node_modules/.bin $PATH # added for NPM (must be last)
+set -gx PATH ./node_modules/.bin $PATH # added for NPM (should be first)
