c90f00e4e06ac542e83127474a1e89c07ae60a0f
Nick Balboni <nbalboni2@gmail.com>
2022-01-20 23:54:26 -0500

    Update install script

--- install.sh
+++ install.sh
@@ -1,8 +1,37 @@
 #!/bin/bash
 
-BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+# This script will replace the contents of the fish configuration folder with
+# symlinks to the contents of this project.
 
-# fish
-ln -s ${BASEDIR}/fish/conf.d/ ~/.config/fish/conf.d
-ln -s ${BASEDIR}/fish/functions/ ~/.config/fish/functions
-ln -s ${BASEDIR}/fish/src/ ~/.config/fish/src
+FISH_HOME="$HOME/.config/fish"
+FOLDERS_TO_LINK=("completions" "conf.d" "functions" "src")
+
+BASE_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+NO_FISH_FOUND_MSG="No fish installation found.
+Is this your fish config directory: $FISH_HOME?"
+REMOVE_EXISTING_FISH_MSG="WARNING: This will clear out your current fish
+config folder ($FISH_HOME).
+
+Press ENTER to continue or CTRL-C to exit..."
+
+# check for fish configuration directory
+if [ ! -d "$FISH_HOME" ]; then
+  echo "$NO_FISH_FOUND_MSG"
+  exit 1
+fi
+
+# clear out existing symlinks
+read -p "$REMOVE_EXISTING_FISH_MSG"
+for fish_folder in ${FOLDERS_TO_LINK[@]}; do
+  if [ -d "$FISH_HOME/$fish_folder" ]; then
+    rm -r "$FISH_HOME/$fish_folder"
+  else
+    rm -f "$FISH_HOME/$fish_folder"
+  fi
+done
+
+# add the symlinks for this project
+for fish_folder in ${FOLDERS_TO_LINK[@]}; do
+  ln -s ${BASE_DIR}/fish/${fish_folder} ~/.config/fish/${fish_folder}
+done
